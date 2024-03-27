3da402ecae4eb4e72eb8ed039ae2a0c5d773fe49
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-03-27 08:15:11 -0400

    up

--- BACKEND/src/database/connection.js
+++ BACKEND/src/database/connection.js
@@ -9,7 +9,6 @@ const config = process.env.NODE_ENV === 'test' ? configutarion.test : configutar
 
 const connection = knex(config)
 
-// Criamos uma variavel com o nome de connection, ela recebe o knex conectado ao arquivo de configuração, o parametro dentro do knex se refere ao modelo de configuração do banco de dados. No nosso  caso estamos utilizando dois modelos de banco, um para produção e outro para realizar os testes. Ele é acionado de acordo com o NPM utilizado.
-
+// Criamos uma variavel com o nome de connection, ela recebe o knex conectado ao arquivo de configuração, o parametro dentro do knex se refere ao modelo de configuração do banco de dados. No nosso  caso estamos utilizando dois modelos de banco, um para produção e outro para realizar os testes. Ele é acionado de acordo com o NPM utilizado. 
 
 module.exports = connection
