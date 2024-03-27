76a882918068f2a9497c36b069f6aea75b62cef3
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-03-27 08:04:13 -0400

    upgrade connection db

--- BACKEND/src/database/connection.js
+++ BACKEND/src/database/connection.js
@@ -5,10 +5,11 @@ const configutarion = require('../../knexfile')
 
 // Criando a conexão com o db e tambem ao db test
 
-const config = process.env.NODE_ENV === test ? configutarion.test : configutarion.development
+const config = process.env.NODE_ENV === 'test' ? configutarion.test : configutarion.development
 
 const connection = knex(config)
 
-// Criamos uma variavel com o nome de connection, ela recebe o knex conectado ao arquivo de configuração, os paramentros nele se referem ao tipo de banco utilizado. Neste nosso caso estamos com o tipo de banco usado para o desenvolvimento da aplicação, quando ficar online o banco utilizado sera do tipo PRODUÇÃO
+// Criamos uma variavel com o nome de connection, ela recebe o knex conectado ao arquivo de configuração, o parametro dentro do knex se refere ao modelo de configuração do banco de dados. No nosso  caso estamos utilizando dois modelos de banco, um para produção e outro para realizar os testes. Ele é acionado de acordo com o NPM utilizado.
+
 
 module.exports = connection
